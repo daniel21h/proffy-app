438a6c01de41debead9359d61b780f5aa210cc2a
daniel21h <hesseldaniel21@gmail.com>
2020-08-07 22:59:53 -0300

    feat: add stack routes

--- App.tsx
+++ App.tsx
@@ -12,6 +12,8 @@ import {
   useFonts
  } from '@expo-google-fonts/poppins'
 
+import AppStack from './src/routes/AppStack';
+
 export default function App() {
   let [fontsLoaded] = useFonts({
     Archivo_400Regular,
@@ -25,7 +27,7 @@ export default function App() {
   } else {
     return (
       <>
-        <Landing />
+        <AppStack />
         <StatusBar style="light" />
       </>
     );
