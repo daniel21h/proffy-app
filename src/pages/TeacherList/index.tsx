5941757337e4d9a569467bf0f2c3408221f2915d
daniel21h <hesseldaniel21@gmail.com>
2020-08-09 22:19:25 -0300

    feat: add and remove favorites

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { View, ScrollView, Text, TextInput } from 'react-native'
 import { Feather } from '@expo/vector-icons'
+import AsyncStorage from '@react-native-community/async-storage'
 import { BorderlessButton, RectButton } from 'react-native-gesture-handler'
 import api from '../../services/api'
 
@@ -11,17 +12,34 @@ import styles from './styles'
 
 function TeacherList() {
   const [isFiltersVisible, setIsFiltersVisible] = useState(false)
+  const [favorites, setFavorites] = useState<number[]>([])
   const [teachers, setTeachers] = useState([])
 
   const [subject, setSubject] = useState('')
   const [week_day, setWeek_day] = useState('')
   const [time, setTime] = useState('')
 
+  function loadFavorites() {
+    AsyncStorage.getItem('favorites').then(response => {
+      if (response) {
+        const favoritedTeachers = JSON.parse(response)
+
+        const favoritedTeachersIds = favoritedTeachers.map((teacher: Teacher) => {
+          return teacher.id
+        })
+
+        setFavorites(favoritedTeachersIds)
+      }
+    })
+  }
+
   function handleToggleFiltersVisible() {
     setIsFiltersVisible(!isFiltersVisible)
   }
 
   async function handleFiltersSubmit() {
+    loadFavorites()
+
     const response = await api.get('classes', {
       params: {
         subject,
@@ -99,7 +117,13 @@ function TeacherList() {
         }}
       >
         {teachers.map((teacher: Teacher) => {
-          return <TeacherItem key={teacher.id} teacher={teacher} />
+          return (
+            <TeacherItem 
+              key={teacher.id} 
+              teacher={teacher} 
+              favorited={favorites.includes(teacher.id)}
+            />
+          )
         })}
       </ScrollView>
     </View>
